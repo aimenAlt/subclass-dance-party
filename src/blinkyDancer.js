4b2495f2a54cfc6814ae016a7e14b6e9adc9096c
aimenAlt <ymnghost@gmail.com>
2019-01-09 20:55:32 -0800

    Update setTimeout to include bind, create spongeDancer.js

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,23 +1,21 @@
 
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
-  // this.oldStep = makeBlinkyDancer.step;
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
-makeBlinkyDancer.prototype.oldStep = makeBlinkyDancer.prototype.step;
+makeBlinkyDancer.prototype.oldStep = makeDancer.prototype.step;
 
 
 makeBlinkyDancer.prototype.step = function() {
-  console.log(this);
-    // call the old version of step at the beginning of any call to this new version of step
-    this.oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    this.$node.toggle();
+  // call the old version of step at the beginning of any call to this new version of step
+  this.oldStep();
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
 };
 
 // var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
--- src/dancer.js
+++ src/dancer.js
@@ -1,25 +1,25 @@
-var makeDancer = function(top, left, timeBetweenSteps){
+var makeDancer = function(top, left, timeBetweenSteps) {
   this.$node = $('<span class="dancer"></span>');
   this.timeBetweenSteps = timeBetweenSteps;
   this.step();
   this.setPosition(top, left);
-}
+};
 
 makeDancer.prototype.step = function () {
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(this.step, this.timeBetweenSteps);  
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);  
 };
 
 makeDancer.prototype.setPosition = function (top, left) {
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    this.$node.css(styleSettings);  
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);  
 };
 
 /*
